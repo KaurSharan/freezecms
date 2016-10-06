58bc8cd42a478c55b494bde9f3247fa784c9a0b9
Sharan <jatti105@gmail.com>
2016-10-06 16:23:16 +0530

    add seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	require 'digest/sha1'
+	before_save :do_hashing
 	has_one :Userdetail
 	has_one :proname
 	has_many :blog
@@ -10,9 +12,17 @@ class User < ActiveRecord::Base
 	validates_format_of :email,:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates_length_of :password, minimum: 6 
 
-	def self.authenticate_user(email,password)
+	def hash_password(password)
+	 		return Digest::SHA1.hexdigest(password)
+	 	end
+
+	 	def do_hashing
+	 		self.password = hash_password(self.password)
+	 	end
+
+	 def self.authenticate_user(email,password)
     	@user = User.find_by_email(email)
-		if @user && @user.password == password
+		if @user && @user.password ==  self.new.hash_password(password)
 			return @user
 		else
 			return false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create([{email:"admin@gmail.com",password:"workingfine",is_active:'1',is_admin:'1',is_seller:'0',created_at:'NOW()',updated_at:'NOW()'}])
